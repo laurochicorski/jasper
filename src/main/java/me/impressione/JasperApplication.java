5022d7dc00c0735b07253926e32ea2cb916d4e00
Lauro Chicorski <laurocoelhoc@gmail.com>
2016-04-05 21:56:12 -0300

    Commit Inicial

---
+++ src/main/java/me/impressione/JasperApplication.java
@@ -0,0 +1,12 @@
+package me.impressione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperApplication.class, args);
+	}
+}
